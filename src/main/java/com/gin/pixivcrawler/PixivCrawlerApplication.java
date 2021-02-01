40edb3ee0633d1ab25cea3b2912f9ab9e74267aa
ginstone <hjg719@139.com>
2021-02-01 17:00:53 +0800

    首次提交

---
+++ src/main/java/com/gin/pixivcrawler/PixivCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.gin.pixivcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixivCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixivCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gin/pixivcrawler/PixivCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gin.pixivcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixivCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
