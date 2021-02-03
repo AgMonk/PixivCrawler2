25abd9dd178eb43b1e64c5fa6703c8fbdd2a91a3
ginstone <hjg719@139.com>
2021-02-03 10:55:18 +0800

    收藏实体完成

--- src/main/java/com/gin/pixivcrawler/utils/pixivUtils/PixivPost.java
+++ src/main/java/com/gin/pixivcrawler/utils/pixivUtils/PixivPost.java
@@ -5,6 +5,7 @@ import com.gin.pixivcrawler.utils.JsonUtil;
 import com.gin.pixivcrawler.utils.pixivUtils.entity.PixivBookmarks;
 import com.gin.pixivcrawler.utils.pixivUtils.entity.details.PixivIllustDetail;
 import com.gin.pixivcrawler.utils.requestUtils.GetRequest;
+import com.gin.pixivcrawler.utils.requestUtils.RequestBase;
 
 import static com.gin.pixivcrawler.utils.JsonUtil.printJson;
 
@@ -22,6 +23,7 @@ public class PixivPost {
      */
     private final static String URL_ILLUST_DETAIL = "https://www.pixiv.net/ajax/illust/%d";
     private final static String URL_USER_BOOKMARKS = "https://www.pixiv.net/ajax/user/%d/illusts/bookmarks";
+    public static final String TIME_COST = " 完成 耗时:{}";
 
 
     /**
@@ -34,6 +36,9 @@ public class PixivPost {
      * @date 2021/2/2 16:45
      */
     public static PixivIllustDetail getIllustDetail(long pid, String cookie) {
+        String msg = "请求作品详情 pid = {}";
+        long start = System.currentTimeMillis();
+        LOG.info(msg, pid);
         String result = GetRequest.create().addCookie(cookie).get(String.format(URL_ILLUST_DETAIL, pid));
         PixivIllustDetail body = null;
         try {
@@ -41,32 +46,37 @@ public class PixivPost {
         } catch (RuntimeException e) {
             LOG.warn("pid = {} {}", pid, e.getMessage());
         }
+        LOG.info(msg + TIME_COST, pid, RequestBase.timeCost(start));
         return body;
     }
 
 
     public static PixivBookmarks getBookmarks(String cookie, long userId, int offset, int limit, String tag) {
+        String msg = "请求收藏作品 userID = {} tag = {} offset = {} limit = {}";
+        long start = System.currentTimeMillis();
+        LOG.info(msg, userId, tag, offset, limit);
         String result = GetRequest.create().addCookie(cookie)
-                .addParam("lang","zh")
-                .addParam("rest","show")
-                .addParam("offset",offset)
-                .addParam("limit",limit)
-                .addParam("tag",tag)
+                .addParam("lang", "zh")
+                .addParam("rest", "show")
+                .addParam("offset", offset)
+                .addParam("limit", limit)
+                .addParam("tag", tag)
                 .get(String.format(URL_USER_BOOKMARKS, userId));
         PixivBookmarks body = null;
         try {
-            body = getBody(result,PixivBookmarks.class);
+            body = getBody(result, PixivBookmarks.class);
         } catch (RuntimeException e) {
             LOG.warn("userId = {} {}", userId, e.getMessage());
         }
+        LOG.info(msg + TIME_COST, userId, tag, offset, limit, RequestBase.timeCost(start));
         return body;
     }
 
 
-    private static <T> T getBody(String result, Class<T> clazz){
+    private static <T> T getBody(String result, Class<T> clazz) {
         JSONObject json = JSONObject.parseObject(result);
         if (!json.getBoolean("error")) {
-            return JSONObject.parseObject(json.getJSONObject("body").toJSONString(),clazz);
+            return JSONObject.parseObject(json.getJSONObject("body").toJSONString(), clazz);
         }
         JsonUtil.printJson(json);
         throw new RuntimeException(json.getString("message"));
--- src/main/java/com/gin/pixivcrawler/utils/pixivUtils/entity/details/PixivIllustDetailInBookmarks.java
+++ src/main/java/com/gin/pixivcrawler/utils/pixivUtils/entity/details/PixivIllustDetailInBookmarks.java
@@ -1,10 +1,46 @@
 package com.gin.pixivcrawler.utils.pixivUtils.entity.details;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gin.pixivcrawler.utils.TimeUtil;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 收藏中的详情
  *
  * @author bx002
  * @date 2021/2/2 17:43
  */
-public class PixivIllustDetailInBookmarks extends PixivDetailBase{
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class PixivIllustDetailInBookmarks extends PixivDetailBase {
+    @JSONField(name = "tags", serialize = false)
+    @JsonIgnore
+    List<String> tagsList;
+    @JSONField(serialize = false)
+    @JsonIgnore
+    String updateDate;
+    @JSONField(serialize = false)
+    @JsonIgnore
+    Long updateSeconds;
+
+    String url;
+
+    public String getUpdateDateTime() {
+        return TimeUtil.second2String(updateSeconds);
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+        this.updateSeconds = getEpochSecond(updateDate);
+    }
+
+    public String getTagString() {
+        return String.join(",", tagsList);
+    }
 }
