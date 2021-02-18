3e66734938cd31a0a4d409c9eedf78ca314a67f5
ginstone <hjg719@139.com>
2021-02-18 10:51:43 +0800

    定时任务线程池

--- src/main/java/com/gin/pixivcrawler/config/TaskExecutorConfig.java
+++ src/main/java/com/gin/pixivcrawler/config/TaskExecutorConfig.java
@@ -3,7 +3,11 @@ package com.gin.pixivcrawler.config;
 import com.gin.pixivcrawler.utils.TasksUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author bx002
@@ -25,7 +29,8 @@ public class TaskExecutorConfig {
     public ThreadPoolTaskExecutor tagExecutor() {
         return TasksUtil.getExecutor("tag", 1);
     }
-@Bean
+
+    @Bean
     public ThreadPoolTaskExecutor searchExecutor() {
         return TasksUtil.getExecutor("search", 1);
     }
@@ -35,4 +40,16 @@ public class TaskExecutorConfig {
         return TasksUtil.getExecutor("main", 1);
     }
 
+    @Bean(name = "scheduler")
+    public TaskScheduler getTaskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(5);
+        taskScheduler.setThreadNamePrefix("scheduler-");
+        taskScheduler.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //调度器shutdown被调用时等待当前被调度的任务完成
+        taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+        //等待时长
+        taskScheduler.setAwaitTerminationSeconds(60);
+        return taskScheduler;
+    }
 }
